01c3e9f1aacb8e86e19b86f44c917c449a50b34f
jochop <jyotichopra.chopra@gmail.com>
2020-06-26 21:43:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-curl --request get https://api.github.com/repos/jochop/MyFirstProject/commits/$GITHUB_SHA > cicd.txt
+echo "Jyoti" > cicd.log
+
+curl --request get https://api.github.com/repos/jochop/MyFirstProject/commits/$GITHUB_SHA >> cicd.txt
 cat cicd.txt | grep -F filename | awk '{print$2}' | cut -d '"' -f 2 >> cicd.log
